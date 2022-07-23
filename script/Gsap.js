2e046c3fd19c90b0c727e89b2f0c0e24f3cec40c
youssef hafnawi <77534098+ymhaah@users.noreply.github.com>
2022-07-24 00:00:53 +0300

    The final shape and the beginning of the code card
    After many experimental designs, the current was settled, and the code card was implemented almost completely,  only experimental colors were used, for animation i used gsap

--- script/Gsap.js
+++ script/Gsap.js
@@ -1,10 +1,35 @@
-gsap.from(".shape-template", {
-	duration: 1,
+let tl = gsap.timeline({ repeat: -1, yoyo: true });
+
+tl.from(".code-font", {
+	duration: 0.5,
+	stagger: 0.1,
+	delay: 0.5,
+	ease: "circ.out",
 	opacity: 0,
-	scale: 0,
-	x: 500,
-	y: 200,
-	// repeat: -1,
-	// yoyo: true,
-	stagger: 1,
-});
+	xPercent: -100,
+	y: -10,
+	scale: 0.5,
+	yoyo: true,
+})
+	.to(".code-font", {
+		duration: 1,
+		delay: 1,
+		stagger: 0.2,
+		yPercent: -200,
+		ease: "sine.out",
+		yoyo: true,
+	})
+	.to(".code-font", {
+		duration: 1.1,
+		delay: 1.1,
+		yPercent: -100,
+		ease: "sine.out",
+		yoyo: true,
+	})
+	.to(".code-font", {
+		duration: 0.9,
+		delay: 0.9,
+		yPercent: 0,
+		ease: "sine.out",
+		yoyo: true,
+	});
